598a75808100cc35c7d2cfdb84add35e3e56b7b5
Redyukov Pavel <dazzgt@gmail.com>
2015-06-23 17:50:00 +0300

    InitCommit

---
+++ DnDMasterTool/MasterToolViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDMasterTool
+{
+    public class MasterToolViewModel
+    {
+        public MasterToolViewModel()
+        {
+            Heroes = new ObservableCollection<Hero>();
+        }
+
+        #region Properties
+
+        public ObservableCollection<Hero> Heroes { get; set; }
+
+        #endregion
+    }
+}
